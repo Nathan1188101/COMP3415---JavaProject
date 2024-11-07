6eeba565ebb4653fce8139572ba415f0d70ebed0
Nathan1188101 <njsecord@lakeheadu.ca>
2024-11-07 17:43:29 -0500

    think I am done phase 2 now

--- compiled/simplechat1/EchoServer.java
+++ compiled/simplechat1/EchoServer.java
@@ -1,6 +1,6 @@
 // This file contains material supporting section 3.7 of the textbook:
 // "Object Oriented Software Engineering" and is issued under the open-source
-// license found at www.lloseng.com 
+// license found at www.lloseng.com
 
 import java.io.*;
 import java.util.Scanner;
@@ -8,7 +8,7 @@ import java.util.Scanner;
 import ocsf.server.*;
 
 /**
- * This class overrides some of the methods in the abstract 
+ * This class overrides some of the methods in the abstract
  * superclass in order to give more functionality to the server.
  *
  * @author Dr Timothy C. Lethbridge
@@ -17,38 +17,37 @@ import ocsf.server.*;
  * @author Paul Holden
  * @version July 2000
  */
-public class EchoServer extends AbstractServer 
+public class EchoServer extends AbstractServer
 {
   //Class variables *************************************************
-  
+
   /**
    * The default port to listen on.
    */
   final public static int DEFAULT_PORT = 5555;
-  
+
   //Constructors ****************************************************
-  
+
   /**
    * Constructs an instance of the echo server.
    *
    * @param port The port number to connect on.
    */
-  public EchoServer(int port) 
+  public EchoServer(int port)
   {
     super(port);
   }
 
-  
+
   //Instance methods ************************************************
-  
+
   /**
    * This method handles any messages received from the client.
    *
    * @param msg The message received from the client.
    * @param client The connection from which the message originated.
    */
-  public void handleMessageFromClient(Object msg, ConnectionToClient client)
-  {
+  public void handleMessageFromClient(Object msg, ConnectionToClient client){
     String newMsg;
     Boolean flag = (Boolean) client.getInfo("flag");//getting the flag from the client
     String message = msg.toString();
@@ -56,7 +55,20 @@ public class EchoServer extends AbstractServer
     //System.out.println("Message received: " + msg + " from " + client);
     //this.sendToAllClients(msg);
 
+    //this is for the first message that the client sends - it must be #login
+    // Check if the client is not logged in and the first message is not #login
+    if ((flag == null || !flag) && !message.startsWith("#login")) {
+      // If this is the first message and it isn't #login, send error and close connection
+      try {
+        client.sendToClient("Error: The first command must be #login. Connection will be terminated.");
+        client.close(); // Terminate the client’s connection
+      } catch (IOException e) {
+        System.out.println("Error closing client connection.");
+      }
+      return; // Stop further processing
+    }
 
+    //if the message starts with #login, then it's a login message
     if(message.startsWith("#login")) {
       //if user hasn't logged in yet i.e flag is false or null (null is for the first time the user logs in)
       if(flag == null || !flag){
@@ -78,6 +90,7 @@ public class EchoServer extends AbstractServer
       return; //returning to avoid the message being sent to all clients, since it's a login message
 
       }
+
 
     String loginId = (String) client.getInfo("loginId");//getting the login id from the client
 
@@ -129,7 +142,7 @@ public class EchoServer extends AbstractServer
   {
     System.out.println("A client has connected to the server: " + client);
   }
-    
+
   /**
    * This method overrides the one in the superclass.  Called
    * when the server starts listening for connections.
@@ -139,7 +152,7 @@ public class EchoServer extends AbstractServer
     System.out.println
       ("Server listening for connections on port " + getPort());
   }
-  
+
   /**
    * This method overrides the one in the superclass.  Called
    * when the server stops listening for connections.
@@ -154,17 +167,17 @@ public class EchoServer extends AbstractServer
     System.out.println(message); //display message on server console
     sendToAllClients(message);//sending message to all clients
   }
-  
+
   //Class methods ***************************************************
-  
+
   /**
-   * This method is responsible for the creation of 
+   * This method is responsible for the creation of
    * the server instance (there is no UI in this phase).
    *
    * @param args The port number to listen on.  Defaults to 5555
    *          if no argument is entered.
    */
-  public static void main(String[] args) 
+  public static void main(String[] args)
   {
     int port = 0; //Port to listen on
 
@@ -176,14 +189,14 @@ public class EchoServer extends AbstractServer
     {
       port = DEFAULT_PORT;// Set port to 5555
     }
-	
+
     EchoServer sv = new EchoServer(port);
-    
-    try 
+
+    try
     {
       sv.listen(); //Start listening for connections
-    } 
-    catch (Exception ex) 
+    }
+    catch (Exception ex)
     {
       System.out.println("ERROR - Could not listen for clients!");
     }
